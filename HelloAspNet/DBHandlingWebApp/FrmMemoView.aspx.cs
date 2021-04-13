67b294e539410ec3816d9dc2bcceca9665cc67b9
choiyeonseong <am3251@naver.com>
2021-04-13 17:01:53 +0900

    0413 ~14장 커밋

---
+++ HelloAspNet/DBHandlingWebApp/FrmMemoView.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DBHandlingWebApp
+{
+    public partial class FrmMemoView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Request["Num"]))
+            {
+                Response.Write("잘못된 요청입니다.");
+                Response.End();
+            }
+            else
+            {
+                var connString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString; //1
+                using (var conn = new SqlConnection(connString))    //1
+                {
+                    if (conn.State == System.Data.ConnectionState.Closed) conn.Open();  //1
+
+                    try
+                    {
+                        SqlCommand cmd = new SqlCommand("ViewMemo", conn);  // 첫번째 파라미터만 변경
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Num", Convert.ToInt32(Request["Num"]));
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            LblNum.Text = Request["Num"];
+                            LblName.Text = reader["Name"].ToString();
+                            LblEmail.Text = reader["Email"].ToString();
+                            LblTitle.Text = reader["Title"].ToString();
+                            LblPostDate.Text = reader["PostDate"].ToString();
+                            LblPostIP.Text = reader["PostIP"].ToString();
+                        }
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HelloAspNet/DBHandlingWebApp/Models/MaximRepository.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DBHandlingWebApp.Models
+{
+    public class MaximRepository
+    {
+        private IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString);
+
+        // 입력
+        public Maxims AddMaxims(Maxims maxim)
+        {
+            string query = @"INSERT INTO Maxims (Name,Contents) values (@Name,@Contents);
+                             SELECT CASE(SCOPE_IDENTITY() AS INT); ";
+            var id = db.Query<int>(query, maxim).Single();
+            maxim.Id = id;
+            return maxim;
+        }
+
+        // 조회
+        
+        // 수정
+        
+        // 삭제
+
+    }
+}
