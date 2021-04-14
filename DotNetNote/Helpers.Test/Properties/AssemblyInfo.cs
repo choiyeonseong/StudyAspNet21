0db5ca6df59b26219ac54f9f30524e44a866c563
choiyeonseong <am3251@naver.com>
2021-04-14 17:27:57 +0900

    0414 19장

---
+++ DotNetNote/Helpers.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helpers.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helpers.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b94f0f5-1e14-42cd-96dc-b5cd5cc8752d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DotNetNote/Helpers.Test/StringLibraryTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Helpers.Test
+{
+    [TestClass]
+    public class StringLibraryTest
+    {
+        [TestMethod]
+        public void CutStringTest()
+        {
+            string strCut = "Hello World, This is a test sentence";
+            int intChar = 15;
+
+            var expected = "Hello World,...";    // 예상되는 값
+            var actual = StringLibrary.CutString(strCut, intChar);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CutStringUnicodeTest()
+        {
+            string strCut = "안녕하세요. 부경대입니다.";
+            int intChar = 9;
+
+            var expected = "안녕하세요....";
+            var actual = StringLibrary.CutStringUnicode(strCut, intChar);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            var expected = 10;
+            var actual = 5 + 5;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
