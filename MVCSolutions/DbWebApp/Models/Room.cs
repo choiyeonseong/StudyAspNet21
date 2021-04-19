8f011653aa5d1413aa495e4bd6c06f24b0ab68a1
choiyeonseong <am3251@naver.com>
2021-04-19 17:03:57 +0900

    0419 MVC TodoList

---
+++ MVCSolutions/DbWebApp/Models/Room.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DbWebApp.Models
+{
+    public class Room
+    {
+        [Key]
+        public int RoomId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<Speaker> Speakers { get; set; }
+    }
+    public class Speaker { 
+        [Key]
+        public int SpeakerId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int RoomId { get; set; }
+
+        public virtual Room Room { get; set; }
+    }
+}
---
+++ MVCSolutions/MvcWebLab.Database/dbo/Tables/Todos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Todos]
+(
+	[Id] INT NOT NULL PRIMARY KEY  IDENTITY, 
+    [Title] NVARCHAR(150) NOT NULL, 
+    [IsDone] BIT NULL, 
+    [CreationDate] DATETIME NULL DEFAULT (GetDate()),
+
+)
